60fbcdb316bc515b1a23a9c370ab286ca3518bc6
somasekhar-boya-milo <somasekharboya9441@gmail.com>
2025-10-15 15:33:18 +0530

    changed dummy project bundle

--- dummy_text/index.js
+++ dummy_text/index.js
@@ -1,8 +1,6 @@
-System.register("chunks:///_virtual/dummy_text",["./Script1.ts","./Script2.ts"],(function(){return{setters:[null,null],execute:function(){}}}));
+System.register("chunks:///_virtual/dummy_text",["./Script1.ts"],(function(){return{setters:[null],execute:function(){}}}));
 
-System.register("chunks:///_virtual/Script1.ts",["./rollupPluginModLoBabelHelpers.js","cc"],(function(t){var r,n,c,o;return{setters:[function(t){r=t.inheritsLoose},function(t){n=t.cclegacy,c=t._decorator,o=t.Component}],execute:function(){var e;n._RF.push({},"68b54MtkvpLEZWxMzdm7ovK","Script1",void 0);var i=c.ccclass;c.property,t("Script1",i("Script1")(e=function(t){function n(){return t.apply(this,arguments)||this}r(n,t);var c=n.prototype;return c.start=function(){},c.update=function(t){},n}(o))||e);n._RF.pop()}}}));
-
-System.register("chunks:///_virtual/Script2.ts",["./rollupPluginModLoBabelHelpers.js","cc"],(function(t){var r,n,c,e;return{setters:[function(t){r=t.inheritsLoose},function(t){n=t.cclegacy,c=t._decorator,e=t.Component}],execute:function(){var o;n._RF.push({},"9a4c5P/nRFMX6WZ6fbmlIal","Script2",void 0);var i=c.ccclass;c.property,t("Script2",i("Script2")(o=function(t){function n(){return t.apply(this,arguments)||this}r(n,t);var c=n.prototype;return c.start=function(){},c.update=function(t){},n}(e))||o);n._RF.pop()}}}));
+System.register("chunks:///_virtual/Script1.ts",["./rollupPluginModLoBabelHelpers.js","cc"],(function(t){var r,c,n,e;return{setters:[function(t){r=t.inheritsLoose},function(t){c=t.cclegacy,n=t._decorator,e=t.Component}],execute:function(){var o;c._RF.push({},"b5d1cvSkWxEk6M46uYeqI70","Script1",void 0);var i=n.ccclass;n.property,t("Script1",i("Script1")(o=function(t){function c(){return t.apply(this,arguments)||this}r(c,t);var n=c.prototype;return n.start=function(){},n.update=function(t){},c}(e))||o);c._RF.pop()}}}));
 
 (function(r) {
   r('virtual:///prerequisite-imports/dummy_text', 'chunks:///_virtual/dummy_text'); 
